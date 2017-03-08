c0247388e492f5cbe542a68dbe921504a8f6736f
Robin Millette <robin@millette.info>
2017-03-08 13:27:12 -0500

    Couchdb listener.

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 'use strict'
 
+// npm
+require('dotenv-safe').load({ allowEmptyValues: true })
+if (!process.env.DB) {
+  console.error('Required env variable: DB.')
+  process.exit(1)
+}
 const chokidar = require('chokidar')
+const CouchdbChangeEvents = require('couchdb-change-events')
 
 const watcher = chokidar.watch('data/**/*.json', { // data
   ignoreInitial: true,
@@ -36,3 +43,36 @@ const cb = (w, path, stats) => {
 }
 
 evs.forEach((x) => watcher.on(x, cb.bind(null, x)))
+
+const options = {
+  host: process.env.HOST || 'localhost',
+  port: process.env.PORT || 5984,
+  protocol: process.env.PROTOCOL || 'http',
+  database: process.env.DB
+}
+
+if (process.env.USERNAME) {
+  options.name = process.env.USERNAME
+}
+
+if (process.env.PASSWORD) {
+  options.password = process.env.PASSWORD
+}
+
+const couchdbEvents = new CouchdbChangeEvents(options)
+
+couchdbEvents.on('data', (data) => {
+  console.log('data:', data)
+})
+
+couchdbEvents.on('couchdb_status', (status) => {
+  console.log('status:', status)
+})
+
+couchdbEvents.on('error', (error) => {
+  console.log('error:', error)
+})
+
+let cnt = 0
+setInterval(() => console.log('couchdbEvents:', ++cnt, Object.keys(couchdbEvents).join()), 6000)
+
