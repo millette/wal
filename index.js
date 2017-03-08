05597717586c6cea6eb727163d36ff497743c17b
Robin Millette <robin@millette.info>
2017-03-08 14:49:18 -0500

    Refactor a bit.

--- index.js
+++ index.js
@@ -9,70 +9,73 @@ if (!process.env.DB) {
 const chokidar = require('chokidar')
 const CouchdbChangeEvents = require('couchdb-change-events')
 
-const watcher = chokidar.watch('data/**/*.json', { // data
-  ignoreInitial: true,
-  followSymlinks: false,
-  alwaysStat: true,
-  // awaitWriteFinish: false
-  awaitWriteFinish: {
-    stabilityThreshold: 2000,
-    pollInterval: 100
+const setupWatcher = (p) => {
+  const watcher = chokidar.watch(p, { // data
+    ignoreInitial: true,
+    followSymlinks: false,
+    alwaysStat: true,
+    // awaitWriteFinish: false
+    awaitWriteFinish: {
+      stabilityThreshold: 2000,
+      pollInterval: 100
+    }
+  })
+
+  watcher.on('ready', () => console.log('Ready!'))
+
+  watcher.on('error', (error) => {
+    if (error.errno === 'ENOSPC') {
+      watcher.close()
+      console.error(`Out of inotify space. Increase it with the following command:
+
+  echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
+
+  The change to this config will remain after a reboot.`)
+      process.exit(1)
+    }
+    console.error(error)
+  })
+
+  const evs = ['add', 'addDir', 'change', 'unlink', 'unlinkDir'] // , 'raw'
+
+  const cb = (w, path, stats) => {
+    console.log(`${w}: ${path}`)
+    if (stats) { console.log('stats:', stats) }
   }
-})
 
-watcher.on('ready', () => console.log('Ready!'))
+  evs.forEach((x) => watcher.on(x, cb.bind(null, x)))
+  return watcher
+}
 
-watcher.on('error', (error) => {
-  if (error.errno === 'ENOSPC') {
-    watcher.close()
-    console.error(`Out of inotify space. Increase it with the following command:
-
-echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
-
-The change to this config will remain after a reboot.`)
-    process.exit(1)
+const setupCouch = () => {
+  const options = {
+    // lastEventId: 'now',
+    host: process.env.HOST || 'localhost',
+    port: process.env.PORT || 5984,
+    protocol: process.env.PROTOCOL || 'http',
+    database: process.env.DB
   }
-  console.error(error)
-})
 
-const evs = ['add', 'addDir', 'change', 'unlink', 'unlinkDir'] // , 'raw'
+  if (process.env.USERNAME) { options.name = process.env.USERNAME }
+  if (process.env.PASSWORD) { options.password = process.env.PASSWORD }
+  const couchdbEvents = new CouchdbChangeEvents(options)
 
-const cb = (w, path, stats) => {
-  console.log(`${w}: ${path}`)
-  if (stats) { console.log('stats:', stats) }
+  couchdbEvents.on('data', (data) => {
+    console.log('data:', data)
+  })
+
+  couchdbEvents.on('couchdb_status', (status) => {
+    console.log('status:', status)
+  })
+
+  couchdbEvents.on('error', (error) => {
+    console.log('error:', error)
+  })
+
+  let cnt = 0
+  couchdbEvents.timer = setInterval(() => console.log('couchdbEvents:', ++cnt, Object.keys(couchdbEvents).join()), 6000)
+  return couchdbEvents
 }
 
-evs.forEach((x) => watcher.on(x, cb.bind(null, x)))
-
-const options = {
-  host: process.env.HOST || 'localhost',
-  port: process.env.PORT || 5984,
-  protocol: process.env.PROTOCOL || 'http',
-  database: process.env.DB
-}
-
-if (process.env.USERNAME) {
-  options.name = process.env.USERNAME
-}
-
-if (process.env.PASSWORD) {
-  options.password = process.env.PASSWORD
-}
-
-const couchdbEvents = new CouchdbChangeEvents(options)
-
-couchdbEvents.on('data', (data) => {
-  console.log('data:', data)
-})
-
-couchdbEvents.on('couchdb_status', (status) => {
-  console.log('status:', status)
-})
-
-couchdbEvents.on('error', (error) => {
-  console.log('error:', error)
-})
-
-let cnt = 0
-setInterval(() => console.log('couchdbEvents:', ++cnt, Object.keys(couchdbEvents).join()), 6000)
-
+const watcher = setupWatcher('data/**/*.json')
+const coucher = setupCouch()
