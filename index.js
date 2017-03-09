96723db6d8cfd02b10f91f014ee49fe8f58a2880
Robin Millette <robin@millette.info>
2017-03-08 20:06:12 -0500

    Proper init and changes queue.

--- index.js
+++ index.js
@@ -1,81 +1,75 @@
 'use strict'
 
 // npm
-require('dotenv-safe').load({ allowEmptyValues: true })
-if (!process.env.DB) {
-  console.error('Required env variable: DB.')
-  process.exit(1)
-}
 const chokidar = require('chokidar')
 const CouchdbChangeEvents = require('couchdb-change-events')
 
 const setupWatcher = (p) => {
-  const watcher = chokidar.watch(p, { // data
+  const watcher = chokidar.watch(p, {
     ignoreInitial: true,
     followSymlinks: false,
     alwaysStat: true,
-    // awaitWriteFinish: false
     awaitWriteFinish: {
       stabilityThreshold: 2000,
       pollInterval: 100
     }
   })
+  return new Promise((resolve, reject) => {
+    const error = (err) => {
+      if (err.errno === 'ENOSPC') {
+        watcher.close()
+        console.error(`Out of inotify space. Increase it with the following command:
 
-  watcher.on('ready', () => console.log('Ready!'))
+    echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
 
-  watcher.on('error', (error) => {
-    if (error.errno === 'ENOSPC') {
-      watcher.close()
-      console.error(`Out of inotify space. Increase it with the following command:
-
-  echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
-
-  The change to this config will remain after a reboot.`)
-      process.exit(1)
+    The change to this config will remain after a reboot.`)
+        reject(err)
+      }
     }
-    console.error(error)
+    watcher.once('ready', () => {
+      watcher.removeListener('error', error)
+      resolve(watcher)
+    })
+    watcher.once('error', error)
   })
-
-  const evs = ['add', 'addDir', 'change', 'unlink', 'unlinkDir'] // , 'raw'
-
-  const cb = (w, path, stats) => {
-    console.log(`${w}: ${path}`)
-    if (stats) { console.log('stats:', stats) }
-  }
-
-  evs.forEach((x) => watcher.on(x, cb.bind(null, x)))
-  return watcher
 }
 
-const setupCouch = () => {
+const setupCouch = (watcher) => {
   const options = {
-    // lastEventId: 'now',
+    // lastEventId: 'abcd...',
     host: process.env.HOST || 'localhost',
     port: process.env.PORT || 5984,
     protocol: process.env.PROTOCOL || 'http',
     database: process.env.DB
   }
-
   if (process.env.USERNAME) { options.name = process.env.USERNAME }
   if (process.env.PASSWORD) { options.password = process.env.PASSWORD }
   const couchdbEvents = new CouchdbChangeEvents(options)
-
-  couchdbEvents.on('data', (data) => {
-    console.log('data:', data)
+  return new Promise((resolve, reject) => {
+    let timer
+    const datas = []
+    const ready = (status) => {
+      if (status === 'connected') {
+        couchdbEvents.removeListener('couchdb_status', ready)
+        clearTimeout(timer)
+        return resolve({ dataQueue, datas, watcher, couchdbEvents })
+      }
+      if (status === 'disconnected') {
+        couchdbEvents.removeListener('couchdb_status', ready)
+        clearTimeout(timer)
+        return reject(new Error('Disconnected at start.'))
+      }
+      console.error('unexpected status:', status)
+    }
+    const dataQueue = (data) => datas.push(data)
+    couchdbEvents.on('data', dataQueue)
+    couchdbEvents.on('couchdb_status', ready)
+    timer = setTimeout(() => {
+      couchdbEvents.removeListener('couchdb_status', ready)
+      reject(new Error('Connect timeout.'))
+    }, 2000)
   })
-
-  couchdbEvents.on('couchdb_status', (status) => {
-    console.log('status:', status)
-  })
-
-  couchdbEvents.on('error', (error) => {
-    console.log('error:', error)
-  })
-
-  let cnt = 0
-  couchdbEvents.timer = setInterval(() => console.log('couchdbEvents:', ++cnt, Object.keys(couchdbEvents).join()), 6000)
-  return couchdbEvents
 }
 
-const watcher = setupWatcher('data/**/*.json')
-const coucher = setupCouch()
+exports.setupWatcher = setupWatcher
+exports.setupCouch = setupCouch
