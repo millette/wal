6574abaac3e7ad2b05f34ba542713c9cc18fe531
Robin Millette <robin@millette.info>
2017-03-08 20:58:26 -0500

    Catch bad listener config.

--- cli.js
+++ cli.js
@@ -7,6 +7,8 @@ if (!process.env.DB) {
   process.exit(1)
 }
 
+const diff = require('lodash.difference')
+
 // self
 const wal = require('.')
 
@@ -55,26 +57,36 @@ const goOn = (x) => {
   return processQueue(x)
 }
 
+const verify = (x) => {
+  const an = x.watcher.eventNames().sort()
+  const bn = x.couchdbEvents.eventNames().sort()
+  const ae = ['add', 'change', 'error', 'unlink']
+  const be = ['couchdb_status', 'data', 'error']
+  const extraA = diff(an, ae)
+  const extraB = diff(bn, be)
+  const missingA = diff(ae, an)
+  const missingB = diff(be, bn)
+  const a = an.filter((name) => x.watcher.listeners(name).length !== 1)
+  const b = bn.filter((name) => x.couchdbEvents.listeners(name).length !== 1)
+
+  if (a.length || b.length || extraA.length || extraB.length || missingA.length || missingB.length) {
+    const err = new Error('Wrong listener config.')
+    err.data = {}
+    if (a.length) { err.data.watcher = a }
+    if (extraA.length) { err.data.watcherExtra = extraA }
+    if (missingA.length) { err.data.watcherMissing = missingA }
+    if (b.length) { err.data.couchdbEvents = b }
+    if (extraB.length) { err.data.couchdbEventsExtra = extraB }
+    if (missingB.length) { err.data.couchdbEventsMissing = missingB }
+    return Promise.reject(err)
+  }
+  return x
+}
+
 wal.setupWatcher('data/**/*.json')
   .then(wal.setupCouch)
   .then(goOn)
-  .then((x) => {
-    console.log(Object.keys(x))
-    const watcher = x.watcher
-    const couchdbEvents = x.couchdbEvents
-    const watcherNames = watcher.eventNames()
-    const couchdbEventsNames = couchdbEvents.eventNames()
-    console.log(watcherNames)
-    console.log(couchdbEventsNames)
-
-    watcherNames.forEach((name) => {
-      console.log(name, watcher.listeners(name).length)
-    })
-
-    couchdbEventsNames.forEach((name) => {
-      console.log(name, couchdbEvents.listeners(name).length)
-    })
-  })
+  .then(verify)
   .catch((e) => {
     console.error(e)
     process.exit(1)
